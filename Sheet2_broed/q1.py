106a4f4fd7e42bdd5179a27a762234cfcad52f36
Edvard Hübinette <edvard@hubinette.me>
2016-10-06 18:50:39 +0200

    Add stuffz

--- Sheet2_broed/q1.py
+++ Sheet2_broed/q1.py
@@ -4,7 +4,6 @@ import random
 import matplotlib.pyplot as plt
 
 class Kohonen(object):
-    # (x,y) for output dimensions, necessary?
     def __init__(self, params):
         self.eta_0 = params['eta_0']
         self.sigma_0 = params['sigma_0']
@@ -12,54 +11,52 @@ class Kohonen(object):
         self.n_input = params['n_input']
         self.n_output = params['n_output']
             
-        self.weights = self.init_weights() 
+        self.weights = self.init_weights()
     
     def init_weights(self):
-        """Initializes weights randomly from the distribution"""
-        return np.array(triangle_points(self.n_output))
-    
+        """Initializes weights randomly"""
+        weights = []
+        for _ in range(self.n_output):
+            x = random.uniform(0,1)
+            y = random.uniform(0,1)
+            weights.append(np.array([x, y]))
+        return np.array(weights)
+
     def update_sigma(self, t):
         self.sigma = self.sigma_0 * math.exp(-(t / self.tau))
     
     def update_eta(self, t):
         self.eta = self.eta_0 * math.exp(-(t / self.tau))
-
-    # BEWARE, BUGS BE HERE
-    def lambda_fn(self, winner, j):
-        """Calculate the neighborhood coefficient for output j"""
-        distance_square = np.linalg.norm(j - winner)
-        return math.exp(-distance_square / (2 * self.sigma**2))
     
-    # OR HERE FOR THAT MATTER
-    def lambda_vector(self, winner):
-        """Returns a vector of neighborhood coefficients"""
-        lambdas = []
-        for w in self.weights:
-            lambdas.append(self.lambda_fn(winner, w))
-        return np.array(lambdas)
+    def lambda_list(self, winner):
+        """Returns a list of neighborhood coefficients"""
+        indices = np.array(range(self.n_output))
+        distances_square = np.square(indices - winner)
+        return np.exp(-distances_square / (2 * self.sigma**2))
     
     def feed(self, pattern):
         """Feed a pattern and update weights"""
         winner = self.winning_neuron_index(pattern)
-        #lambdas = self.lambda_vector(self.weights[winner])
-        #delta_w = []
-        for j in range(self.n_output):
-            diff = pattern - self.weights[winner]
-            #delta_w = self.eta * lambdas.item(j) * diff
-            delta_w = self.eta * diff
-            self.weights.put(j, self.weights.item(j) + delta_w)
-            
-        #self.weights += np.array(delta_w)
+        delta_w = []
+        lambdas = self.lambda_list(winner)
+        for i in range(self.n_output):
+            diff = pattern - self.weights[i]
+            delta_w.append(self.eta * lambdas[i] * diff)
+        self.weights += np.array(delta_w)
 
     def winning_neuron_index(self, pattern):
         """Returns the index of the winning neuron"""
-        min = np.linalg.norm(self.weights[0])
+        winning_index = 0
+        min_length = np.linalg.norm(pattern - self.weights[0])
         for i in range(self.n_output):
-            if np.linalg.norm(pattern - self.weights[i]) < min:
-                min = i
-        return min
+            len = np.linalg.norm(pattern - self.weights[i])
+            if len < min_length:
+                min_length = len
+                winning_index = i
+        return winning_index
 
 def triangle_points(n_points):
+    """Draws points from the uniform, triangular distribution"""
     x = np.array([1, 0])
     y = np.array([1, np.sqrt(3)])
     y = y / np.linalg.norm(y)
@@ -68,12 +65,19 @@ def triangle_points(n_points):
     for _ in range(n_points):
         a1 = np.random.uniform()
         a2 = np.random.uniform()
+        # transform points in upper triangle to lower
         if (a1 + a2 > 1):
             a1 = 1 - a1
             a2 = 1 - a2
         p = a1*x + a2*y    
         points.append(a1*x + a2*y)
     return points
+
+def draw_triangle():
+    """Draws the equilateral triangle"""
+    plt.plot([0, 1], [0, 0], color='b', linestyle='-')
+    plt.plot([0, 0.5], [0, np.sqrt(3)/2], color='b', linestyle='-')
+    plt.plot([0.5, 1], [np.sqrt(3)/2, 0], color='b', linestyle='-')
 
 if __name__ == '__main__':
     # model parameters
@@ -92,20 +96,33 @@ if __name__ == '__main__':
     # generate data points
     data = triangle_points(1000)
     
-    net = Kohonen(params)
-    plt.scatter(*zip(*net.weights), marker='*', color='b')
-    
-    for i in range(1, t_order):
-        net.update_sigma(i)
-        net.update_eta(i)
-        net.feed(random.choice(data))
- 
-    plt.scatter(*zip(*net.weights), marker='*', color='g')
-    net.sigma = sigma_conv
-    net.eta = eta_conv
-    for i in range(1, t_conv):
-        net.feed(random.choice(data))  
- 
-    plt.scatter(*zip(*net.weights), marker='*', color='r')
-    #plt.scatter(*zip(*data))
-    plt.show()
+    for run in range(1,2):
+        if run == 1:
+            params['sigma_0'] = 5
+        net = Kohonen(params)
+
+        # ordering phase
+        for i in range(1, t_order):
+            net.update_sigma(i)
+            net.update_eta(i)
+            net.feed(random.choice(data)) 
+        # create plot
+        plt.figure() 
+        plt.plot(*zip(*net.weights), marker='*')
+        draw_triangle()
+        plt.savefig('1{}_order.png'.format('a' if run == 0 else 'b'), 
+                    bbox_inches='tight')
+        
+        # set parameters for convergence phase
+        net.sigma = sigma_conv
+        net.eta = eta_conv
+        # convergence phase
+        for i in range(1, t_conv):
+            net.feed(random.choice(data))  
+        
+        # create plot
+        plt.figure()
+        plt.plot(*zip(*net.weights), marker='*')
+        draw_triangle()
+        plt.savefig('1{}_conv.png'.format('a' if run == 0 else 'b'), 
+                    bbox_inches='tight')
